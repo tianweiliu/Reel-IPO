b61c7832aac26654a31bbe1058c0bd802fc41f29
tianweiliu <tianwei.liu89@gmail.com>
2013-01-21 20:40:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
     -- by Tianwei Liu (tianwei.liu89@gmail.com)
   */
   include("ipo.php");
-	if (empty($_GET["semester"]))
-		$semester = "f12";
-	else
-		$semester = $_GET["semester"];
-	$STOCK_URL = "http://www2.tech.purdue.edu/cgt/Courses/cgt411/COGENT/".$semester."IPO_files/sheet001.htm";
-	$mode = "auto";
-	if (!empty($_GET["key"])) {
-		$key = $_GET["key"];
-		$mode = "manual";
-	}
-	if (empty($_GET["group_id"]) || empty($_GET["jsonp"]))
-		header("Location:admin.php");
-	else {
-		echo $_GET["jsonp"]."(".json_encode(RetrieveIPO($_GET["group_id"], $semester, $key, $mode)).")";
-	}
+  if (empty($_GET["semester"]))
+  	$semester = "f12";
+  else
+  	$semester = $_GET["semester"];
+  $STOCK_URL = "http://www2.tech.purdue.edu/cgt/Courses/cgt411/COGENT/".$semester."IPO_files/sheet001.htm";
+  $mode = "auto";
+  if (!empty($_GET["key"])) {
+	$key = $_GET["key"];
+	$mode = "manual";
+  }
+  if (empty($_GET["group_id"]) || empty($_GET["jsonp"]))
+	header("Location:admin.php");
+  else {
+	echo $_GET["jsonp"]."(".json_encode(RetrieveIPO($_GET["group_id"], $semester, $key, $mode)).")";
+  }
 ?>
